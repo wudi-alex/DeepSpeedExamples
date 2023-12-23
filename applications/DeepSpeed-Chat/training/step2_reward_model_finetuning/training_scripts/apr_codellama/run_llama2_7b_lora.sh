0fb563c98db1ce8f6d223c2c48185c54c05e824e
dw <1798827828@qq.com>
2023-12-23 00:12:09 -0500

    fix

---
+++ applications/DeepSpeed-Chat/training/step2_reward_model_finetuning/training_scripts/apr_codellama/run_llama2_7b_lora.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+# Copyright (c) Microsoft Corporation.
+# SPDX-License-Identifier: Apache-2.0
+
+# DeepSpeed Team
+OUTPUT=$1
+ZERO_STAGE=$2
+if [ "$OUTPUT" == "" ]; then
+    OUTPUT=./output_step2_codellama_7b_epoch5_lr9.65e-6
+fi
+if [ "$ZERO_STAGE" == "" ]; then
+    ZERO_STAGE=3
+fi
+mkdir -p $OUTPUT
+
+deepspeed main.py \
+   --data_path /projects/ksun3/dwu25/apr_datasets_processing/coconut/data/apr_rm_coconut \
+   --data_split 1 \
+   --model_name_or_path codellama/CodeLlama-7b-hf \
+   --per_device_train_batch_size 16 \
+   --per_device_eval_batch_size 16 \
+   --max_seq_len 600 \
+   --learning_rate 9.65e-6 \
+   --weight_decay 0.1 \
+   --num_padding_at_beginning 0 \
+   --num_train_epochs 5 \
+   --gradient_accumulation_steps 1 \
+   --lr_scheduler_type cosine \
+   --num_warmup_steps 0 \
+   --seed 1234 \
+   --gradient_checkpointing \
+   --zero_stage $ZERO_STAGE \
+   --deepspeed \
+   --offload \
+   --lora_dim 128 \
+   --lora_module_name "layers." \
+   --output_dir $OUTPUT \
+   &> $OUTPUT/training.log
