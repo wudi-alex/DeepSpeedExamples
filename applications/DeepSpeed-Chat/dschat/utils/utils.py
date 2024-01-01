eacce40fbd1f1a28c1be06356ea3a0d5f32ce3c5
dw <1798827828@qq.com>
2023-12-31 21:58:18 -0500

    fix

--- applications/DeepSpeed-Chat/dschat/utils/utils.py
+++ applications/DeepSpeed-Chat/dschat/utils/utils.py
@@ -75,9 +75,11 @@ class ExponentialMovingAverage:
 def get_tokenizer(model_name_or_path, fast_tokenizer=True):
     tokenizer = AutoTokenizer.from_pretrained(
         model_name_or_path, fast_tokenizer=fast_tokenizer)
-    tokenizer.pad_token = tokenizer.eos_token
-    # make sure tokenizer is right pad in our logic
-    tokenizer.padding_side = 'right'
+    if tokenizer.pad_token is None:
+        # assert tokenizer.eos_token is not None
+        # tokenizer.add_special_tokens({'pad_token': tokenizer.eos_token})
+        tokenizer.add_special_tokens({'pad_token': '[PAD]'})
+        tokenizer.padding_side = 'right'
     return tokenizer
 
 
