0b3b7c37384b2c6ab6f7687fe4f4f3f5718dec7c
dw <1798827828@qq.com>
2023-12-31 19:35:47 -0500

    fix

--- applications/DeepSpeed-Chat/training/step1_supervised_finetuning/evaluation_scripts/run_prompt.sh
+++ applications/DeepSpeed-Chat/training/step1_supervised_finetuning/evaluation_scripts/run_prompt.sh
@@ -7,5 +7,5 @@
 # You can provide two models to compare the performance of the baseline and the finetuned model
 export CUDA_VISIBLE_DEVICES=0
 python prompt_eval.py \
-    --model_name_or_path_baseline XXX \
-    --model_name_or_path_finetune XXX
+    --model_name_or_path_baseline codellama/CodeLlama-7b-hf \
+    --model_name_or_path_finetune /projects/ksun3/dwu25/trained_models/ds_apr_sft
