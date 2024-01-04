f4bb07f7a20e865182ba7c7a0c263b5fe0174779
dw <1798827828@qq.com>
2024-01-04 00:04:13 -0500

    fix

--- applications/DeepSpeed-Chat/training/step2_reward_model_finetuning/training_scripts/run_codellama_7b_apr_rm_coconut_lora.sh
+++ applications/DeepSpeed-Chat/training/step2_reward_model_finetuning/training_scripts/run_codellama_7b_apr_rm_coconut_lora.sh
@@ -35,8 +35,8 @@ deepspeed ../main.py \
    --data_path /projects/ksun3/dwu25/apr_datasets_processing/coconut/data/apr_rlhf_coconut \
    --data_split 0,4,6 \
    --model_name_or_path codellama/CodeLlama-7b-hf \
-   --per_device_train_batch_size 64 \
-   --per_device_eval_batch_size 128 \
+   --per_device_train_batch_size 16 \
+   --per_device_eval_batch_size 32 \
    --max_seq_len 600 \
    --learning_rate 5e-6 \
    --weight_decay 0.1 \
@@ -47,7 +47,7 @@ deepspeed ../main.py \
    --num_warmup_steps 0 \
    --seed 1234 \
    --gradient_checkpointing \
-   --zero_stage 3 \
+   --zero_stage 2 \
    --deepspeed \
    --offload \
    --lora_dim 8 \
